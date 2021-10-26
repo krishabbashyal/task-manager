90f3535ea714f56b1e210fd1ed46110d91a429de
Krishab Bashyal <krishab2002@gmail.com>
2021-10-25 19:34:34 -0500

    Made changes to the CSS of the Register and Login Pages

--- mainProjectDir/mainProjectDir/settings.py
+++ mainProjectDir/mainProjectDir/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-p4dhk+=i)h(l!k4cgflzd9-z@38zy6^j#nx+z52ok7)#*-)a6f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
